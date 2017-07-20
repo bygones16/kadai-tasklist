ae84990641bbf514214b01a51888884861d1e907
jiro.kirameki <rakva492@gmail.com>
2017-07-20 09:46:10 +0000

    kadai-tasklist

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -14,13 +14,13 @@ class TasksController < ApplicationController
   def create
     @task = Task.new(task_params)
 
-    　if @task.save
-      　flash[:success] = 'Task が正常に投稿されました'
-      　redirect_to @task
-    　else
-      　flash.now[:danger] = 'Task が投稿されませんでした'
-     　　 render :new
-     　end
+    if @task.save
+      flash[:success] = 'Task が正常に投稿されました'
+      redirect_to @task
+    else
+      flash.now[:danger] = 'Task が投稿されませんでした'
+      render :new
+     end
   end
 
   def edit
